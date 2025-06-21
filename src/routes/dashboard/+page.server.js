3aec26c459e91e97d38a548a5ee0384a7dfae8de
SoloPHP <vedastudio@gmail.com>
2025-06-21 14:35:44 +0300

    fix

--- src/routes/dashboard/+page.server.js
+++ src/routes/dashboard/+page.server.js
@@ -7,25 +7,3 @@ export const load = async (event) => {
 		user
 	};
 };
-
-// src/routes/logout/+page.server.js
-import { redirect } from '@sveltejs/kit';
-import { clearAuthCookies } from '$lib/server/auth-utils.js';
-import { callPhpApi } from '$lib/server/api.js';
-
-export const actions = {
-	default: async ({ cookies, fetch }) => {
-		const token = cookies.get('jwt_token');
-
-		if (token) {
-			try {
-				await callPhpApi('/auth/logout', 'POST', {}, token, fetch);
-			} catch (error) {
-				console.warn('PHP API logout error:', error);
-			}
-		}
-
-		clearAuthCookies(cookies);
-		throw redirect(302, '/login');
-	}
-};
